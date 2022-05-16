1e6a17af7ea9cb9c82e19409627838b6d0770c95
qrsofVictorQuintero <victor.qperez@hotmail.com>
2022-05-16 16:19:16 -0500

    test1

---
+++ appangular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
