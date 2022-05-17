ac9c5c1b4e5e75b8c78f9ba984a9ab7008722398
qrsofVictorQuintero <victor.qperez@hotmail.com>
2022-05-17 11:08:19 -0500

    SharedComponent Add ingredient

--- appangular/src/app/recipes/recipes-list/recipes-list.component.ts
+++ appangular/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes.module';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Tacos Dorados',
+      'Tortilla dorada enrollada',
+      'https://www.cardamomo.news/__export/1609534137205/sites/debate/img/2021/01/01/receta_de_tacos_dorados_rellenos_de_papax_econxmicos_y_deliciosos_crop1609534119847.jpeg_242310155.jpeg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- appangular/src/app/shopping-list/shopping-list.component.ts
+++ appangular/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients : ingredient[] = [
+    new ingredient('Apple', 5),
+    new ingredient('Tomato', 10),
+  ];
   constructor() { }
 
   ngOnInit(): void {
